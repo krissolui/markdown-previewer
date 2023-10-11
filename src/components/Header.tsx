59a7797237bfb41349ba717d6484ff69c885aafa
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-11 19:30:46 -0400

    completed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Header = () => {
-	return <div>Header</div>;
+interface IHeaderProps {
+	onToggleGuide: () => void;
+}
+
+const Banner = styled.header`
+	display: flex;
+	justify-content: space-between;
+	background-color: rgb(40, 44, 52);
+	color: #ffffff;
+	padding: 10px 20px;
+`;
+
+const Title = styled.h1`
+	margin: 0;
+`;
+
+const Button = styled.button`
+	height: 100%;
+	margin: auto 0;
+	border: 0;
+	border-radius: 5px;
+	background-color: #40c2ff;
+	&:hover {
+		background-color: #40c2ffba;
+		transition: background-color 0.2s ease 0s;
+		cursor: pointer;
+	}
+	color: rgb(40, 44, 52);
+	padding: 10px;
+	font-size: 1rem;
+`;
+
+const Header = ({ onToggleGuide }: IHeaderProps) => {
+	return (
+		<Banner>
+			<Title>Markdown Previewer</Title>
+			<Button onClick={onToggleGuide}>Markdown Cheat Sheet</Button>
+		</Banner>
+	);
 };
 
 export default Header;
