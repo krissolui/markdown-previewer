7da1ab827ec1eca6d054364c7ebb654db8311c06
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-11 19:31:27 -0400

    added components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
 import './App.css';
+import MarkdownGuide from './components/MarkdownGuide';
+import Header from './components/Header';
+import MarkdownInput from './components/MarkdownInput';
+import MarkdownOutput from './components/MarkdownOutput';
+import styled from 'styled-components';
+
+const Display = styled.div`
+	height: 100%;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	flex: 1;
+`;
 
 function App() {
-	return <>App</>;
+	const [showGuide, setShowGuide] = useState<boolean>(false);
+	const [input, setInput] = useState<string>('# Hello World');
+
+	const onToggleGuide = () => {
+		setShowGuide(!showGuide);
+	};
+
+	return (
+		<>
+			<Header onToggleGuide={onToggleGuide} />
+			{showGuide && <MarkdownGuide />}
+			<Display>
+				<MarkdownInput input={input} setInput={setInput} />
+				<MarkdownOutput input={input} />
+			</Display>
+		</>
+	);
 }
 
 export default App;
