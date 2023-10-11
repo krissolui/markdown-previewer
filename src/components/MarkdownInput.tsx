fbc78c52af727329098fc21db06f3a3432358113
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-11 19:31:11 -0400

    completed display sections

--- src/components/MarkdownInput.tsx
+++ src/components/MarkdownInput.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const MarkdownInput = () => {
-	return <div>MarkdownInput</div>;
+interface IMarkdownInput {
+	input: string;
+	setInput: (input: string) => void;
+}
+
+const Container = styled.div`
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+	padding: 20px;
+`;
+
+const TextArea = styled.textarea`
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+	padding: 10px;
+	border: none;
+	outline: none;
+	resize: none;
+	font-family: inherit;
+	font-size: inherit;
+`;
+
+const MarkdownInput = ({ input, setInput }: IMarkdownInput) => {
+	const handleChangeInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInput(e.target.value);
+	};
+
+	return (
+		<Container>
+			<TextArea
+				autoComplete="false"
+				value={input}
+				onChange={handleChangeInput}
+			/>
+		</Container>
+	);
 };
 
 export default MarkdownInput;
--- src/components/MarkdownOutput.tsx
+++ src/components/MarkdownOutput.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import Markdown from 'react-markdown';
+import styled from 'styled-components';
 
-const MarkdownOutput = () => {
-	return <div>MarkdownOutput</div>;
+interface IMarkdownOutput {
+	input: string;
+}
+
+const Container = styled.div`
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+	padding: 20px;
+	overflow: scroll;
+	background-color: rgb(249, 249, 249);
+	border-left: 1px solid rgb(221, 221, 221);
+`;
+
+const MarkdownOutput = ({ input }: IMarkdownOutput) => {
+	return (
+		<Container>
+			<Markdown>{input}</Markdown>
+		</Container>
+	);
 };
 
 export default MarkdownOutput;
